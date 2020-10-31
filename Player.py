781837e2d30159df0734635c5ebbbbc0b028de2e
caffeine98 <49334026+caffeine98@users.noreply.github.com>
2020-10-31 14:20:38 -0700

    Created Player.py
    PLayer class. Still incomplete

---
+++ Player.py
@@ -0,0 +1,30 @@
+import import_ipynb
+import room.ipynb
+
+class Player:
+    room_ID = 0
+    def __init__(self, name, age, health, items):
+        self.name = name
+        self.age = age
+        self.health = health
+        self.items = []
+
+    def get_name(self):
+        return self.name
+
+    def set_name(self, name):
+        self.name = name
+
+    def get_age(self):
+        return self.age
+
+    def set_age(self, age):
+        self.age = age
+
+    def get_health(self):
+        return self.health
+
+    def set_health(self, health):
+        self.health = health
+
+    currentroom = room(room_ID)
