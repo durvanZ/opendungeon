0e5e14ca90f394f64f7a1b8a48283c36098ff49c
caffeine98 <49334026+caffeine98@users.noreply.github.com>
2020-10-31 20:15:51 -0700

    Update Cipher.py

--- Cipher.py
+++ Cipher.py
@@ -9,17 +9,18 @@ class Cipher:
         from codecs import encode
         return encode(str, 'rot13')
     
-words = ['ghosts', 'halloween', 'spirits', 'october', 'scary', 'spooky', 'spooktober']
-print('The following is an encrypted text. Decrypt it to continue...')
-myString = random.choice(words)
-ciphered = Cipher().caesar(myString)
+    def run(self):
+        words = ['ghosts', 'halloween', 'spirits', 'october', 'scary', 'spooky', 'spooktober']
+        print('The following is an encrypted text. Decrypt it to continue...')
+        myString = random.choice(words)
+        ciphered = Cipher().caesar(myString)
 
-correct = False
-while correct == False:
-    print(ciphered)
-    answer = input("Enter your decrypted message : ")
-    if (Cipher().caesar(answer).lower().split() == ciphered.lower().split()): 
-        print("Congratulations, you may continue.")
-        correct = True
-    else:
-        print("Wrong Answer\n")
+        correct = False
+        while correct == False:
+            print(ciphered)
+            answer = input("Enter your decrypted message : ")
+            if (Cipher().caesar(answer).lower().split() == ciphered.lower().split()): 
+                print("Congratulations, you may continue.")
+                correct = True
+            else:
+                print("Wrong Answer\n")
